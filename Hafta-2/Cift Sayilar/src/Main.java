dbe159cbd2802527d701f810797298c90ff7e76e
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-26 19:07:40 +0300

    CiftSayilar

---
+++ Hafta-2/Cift Sayilar/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] list = {2, 5, 2, 2, 8, 5, 3, 11, 9, 8, 2, 8, 10, 10, 12, 12};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if (i != j && list[i] == list[j] && i % 2 == 0) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+
+
+    }
+}
