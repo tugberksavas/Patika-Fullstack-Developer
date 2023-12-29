ba125975c943bb85314650e406f20c31864709f8
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-29 13:56:49 +0300

    guncel.1

--- Hafta-2/MaasHesaplayici/src/Main.java
+++ Hafta-2/MaasHesaplayici/src/Main.java
@@ -1,17 +1,23 @@
-public class Main {
+import java.text.DecimalFormat;
 
+public class Main {
 
 
     public static void main(String[] args) {
 
-    Employee employee = new Employee("tugberk",1000,50,2020);
-        System.out.println(employee.toString());
-        System.out.println(employee.raiseSalary(employee));
-        System.out.println(employee.bonus(employee.workHours));
-        System.out.println(employee.tax(employee.salary));
+        Employee employee = new Employee("tugberk", 2000, 45, 1985);
+        double total =(employee.salary +employee.bonus(employee.workHours))-employee.tax(employee.salary);
+        double totalMaas = total+employee.raiseSalary(employee);
 
-
-
+        System.out.println("adi: " + employee.name);
+        System.out.println("maas: " + employee.salary);
+        System.out.println("calisma saati: " + employee.workHours);
+        System.out.println("baslangic yili: " + employee.hireYear);
+        System.out.println("vergi: " + employee.tax(employee.salary));
+        System.out.println("bonus: " + employee.bonus(employee.workHours));
+        System.out.println("maas artisi: " + employee.raiseSalary(employee));
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + total);
+        System.out.println("toplam maas: "+ totalMaas );
 
 
 
