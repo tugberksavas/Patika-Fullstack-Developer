63cb1606b919e554cd3d81e7e86676396c0210c9
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2024-01-13 12:54:13 +0300

    4.odev

---
+++ hafta-5/SQL Homework/4odev.sql
@@ -0,0 +1,9 @@
+--SELECT DISTINCT replacement_cost FROM film
+
+--SELECT COUNT (DISTINCT replacement_cost) FROM film
+
+--SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating = 'G'
+
+--SELECT * FROM country WHERE Length(country) = 5; 
+
+SELECT COUNT(*) FROM city WHERE city ILIKE '%r'
